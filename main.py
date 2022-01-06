e5a7f1d74a3b6f5be5ce50c1d561368b90086c27
ArtemMartynov <artem.martinov99@mail.ru>
2022-01-06 21:25:03 +0500

    fifth web

--- main.py
+++ main.py
@@ -10,8 +10,5 @@ def index():
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     main.run(host='0.0.0.0', port=8080, debug=True)
-=======
-    main.run(host = '127.0.0.1', port = 8080, debug=True)
->>>>>>> 36033fad04b273e173c8910719f99e84b2d40559
+
