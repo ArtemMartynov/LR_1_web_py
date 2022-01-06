352873ff98690aaaf7ba3f83507e2ecab0346acb
ArtemMartynov <artem.martinov99@mail.ru>
2022-01-06 21:20:09 +0500

    fourth web

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 #Веб-сервере на питоне
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 main = Flask(__name__)
 
 
 @main.route('/')
-@main.route('/home')
 def index():
-    return '<html>\n<body>\n<h1>Всем привет, я только проснулся</h1>\n<head>\n<h2>Da</h2>\n</head>\n</body>\n</html>'
-
-
-@main.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'User Name: '+ name + '\nid:' + str(id)
-
-@main.route('/Geppa')
-def Geppa():
-    return '<html>\n<body>\n<h1>Добро пожаловать в Жеппу</h1>\n<h6>Снова...</h6>\n</body></html>'
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main.run(debug=True)
+    main.run(host='0.0.0.0', port=8080, debug=True)
